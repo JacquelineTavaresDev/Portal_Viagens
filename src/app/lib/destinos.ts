8f492f50eae5bb3bfc78abde65a2083921c05fcb
JacquelineTavares <jacqueline.tavares1996@gmail.com>
2026-01-05 20:23:59 -0300

    Add files via upload

---
+++ src/app/lib/destinos.ts
@@ -0,0 +1,28 @@
+import { Destino } from "../types/types";
+
+export const destinos: Destino[] = [
+  {
+    id: "1",
+    nome: "Paris",
+    imagem: "/paris.jpg",
+    descricao: "Paris é conhecida como a cidade da luz, famosa por sua cultura, gastronomia e monumentos históricos."
+  },
+  {
+    id: "2",
+    nome: "Roma",
+    imagem: "/roma.jpg",
+    descricao: "Roma é uma cidade histórica com ruínas antigas, museus e uma rica herança cultural."
+  },
+  {
+    id: "3",
+    nome: "Rio de Janeiro",
+    imagem: "/rio.jpg",
+    descricao: "O Rio de Janeiro é famoso por suas praias, paisagens naturais e o Cristo Redentor."
+  },
+  {
+    id: "4",
+    nome: "Tóquio",
+    imagem: "/toquio.jpg",
+    descricao: "Tóquio mistura tradição e tecnologia, oferecendo uma experiência cultural única."
+  }
+];
