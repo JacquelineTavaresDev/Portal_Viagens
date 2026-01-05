9a8795b8e4dba981cf8c931a9446c0913ae9bcac
JacquelineTavares <jacqueline.tavares1996@gmail.com>
2026-01-05 20:26:16 -0300

    Add files via upload

---
+++ src/app/destinos/id/page.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import { destinos } from "../../lib/destinos";
+import styles from "./page.module.css";
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function DestinoDetalhe({ params }: Props) {
+  const { id } = await params;
+
+  const destino = destinos.find((d) => d.id === id);
+
+  if (!destino) {
+    return <h1>Destino não encontrado</h1>;
+  }
+
+  return (
+    <main className={styles.container}>
+      <h1 className={styles.titulo}>{destino.nome}</h1>
+
+      <div className={styles.imagemWrapper}>
+        <Image
+          src={destino.imagem}
+          alt={destino.nome}
+          fill
+          className={styles.imagem}
+          priority
+        />
+      </div>
+
+      <p className={styles.descricao}>{destino.descricao}</p>
+    </main>
+  );
+}
